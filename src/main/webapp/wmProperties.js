ad8ac02681453efb78dc40c1a3543d4618707fff
root <root@61949b3d2f95>
2017-07-03 13:43:54 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ADkYcvCKLE",
+  "homePage" : "Main",
+  "name" : "Auto_ADkYcvCKLE",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
